b25525ec813adf11361a8c5d532a3e4cbe1ab84e
Danny Avery <danny.w.avery@gmail.com>
2018-01-09 13:47:43 -0700

    Firebase authentication + signIn & signOut methods

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,8 +11,43 @@
 				<input type="password" class="form-control" id="password" placeholder="Enter password">
 			</div>
 
-			<button class="btn btn-primary" type="button">Sign in</button>
-			<button class="btn btn-danger" type="button">Log out</button>
+			<button class="btn btn-primary" 
+							type="button"
+							@click.prevent="signIn">Sign in</button>
+			<button class="btn btn-danger"
+							type="button"
+							@click.prevent="signOut">Log out</button>
 		</form>
 	</div>
 </template>
+
+<script>
+	import Firebase from 'firebase'
+
+	export default {
+		methods: {
+			signIn () {
+				var email = document.getElementById('email').value;
+				var password = document.getElementById('password').value;
+
+				Firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+					var errorCode = error.code;
+					var errorMessage = error.message;
+
+					if (errorCode === 'auth/wrong-password') {
+						alert('Wrong password')
+					} else {
+						alert(errorMessage)
+					}
+				});
+			}, // signIn
+			signOut () {
+				Firebase.auth().signOut().then(function() {
+					alert('logged out')
+				}).catch(function(error) {
+					alert(error)
+				});
+			} // signOut
+		} // methods
+	}
+</script>
