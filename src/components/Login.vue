2cb4b697171e3196ed3647e43cf1e8c58b8add96
Danny Avery <danny.w.avery@gmail.com>
2018-01-09 13:26:25 -0700

    Start login comp

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="row">
+		<form>
+			<div class="form-group">
+				<label>Email</label>
+				<input type="email" class="form-control" id="email" placeholder="Enter email">
+			</div>
+
+			<div class="form-group">
+				<label>Password</label>
+				<input type="password" class="form-control" id="password" placeholder="Enter password">
+			</div>
+
+			<button class="btn btn-primary" type="button">Sign in</button>
+			<button class="btn btn-danger" type="button">Log out</button>
+		</form>
+	</div>
+</template>
