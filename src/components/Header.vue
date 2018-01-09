06dfa5302fd7d653682db0a1b5da024f1aab8c6c
Danny Avery <danny.w.avery@gmail.com>
2018-01-09 08:32:08 -0700

    Header & Footer Templating

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+	<header class="row">
+		<nav class="navbar navbar-toggleable-md navbar-light">
+			<a href="/" class="navbar-brand">PIZZA PLANET</a>
+			<ul class="nabar-nav">
+				<li class="nav-item">
+					<a href="" class="nav-link">home</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">menu</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
