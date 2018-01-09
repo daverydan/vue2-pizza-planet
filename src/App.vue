dedf8a457fd285c32b004d187d3ad46090148c10
Danny Avery <danny.w.avery@gmail.com>
2018-01-09 08:43:53 -0700

    Import & Display Components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
-    
+		<div class="container">
+			<pp-header></pp-header>
+		</div>
+		<div class="container">
+			<pp-home></pp-home>
+		</div>
+		<div class="container">
+			<pp-footer></pp-footer>
+		</div>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+
 export default {
-  
+	components: {
+		ppHeader: Header,
+		ppFooter: Footer,
+		ppHome: Home
+	}
 }
 </script>
 
