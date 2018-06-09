e63223ae25e27bfa8e006108b132989e24fb1fdb
Danny Avery <danny.w.avery@gmail.com>
2018-06-09 09:20:56 -0600

    Nested router view for About child routes

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,14 @@
 <template>
-	<h1>About Us</h1>
+	<div>
+		<p>Click on the links below for more info:</p>
+		<nav class="navbar navbar-toggleable-md navbar-light">
+			<ul class="navbar-nav">
+				<router-link to="/history" tag="li"><a class="nav-link">history</a></router-link>
+				<router-link to="/contact" tag="li"><a class="nav-link">contact</a></router-link>
+				<router-link to="/ordering-guide" tag="li"><a class="nav-link">ordering guide</a></router-link>
+				<router-link to="/delivery" tag="li"><a class="nav-link">delivery</a></router-link>
+			</ul>
+		</nav>
+		<router-view></router-view>
+	</div>
 </template>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="row">
 		<nav class="navbar navbar-toggleable-md navbar-light">
-			<ul class="nabar-nav">
+			<ul class="navbar-nav">
 				<router-link to="/" tag="li"><a class="nav-link">home</a></router-link>
 				<router-link to="/about" tag="li"><a class="nav-link">about</a></router-link>
 				<router-link to="/admin" tag="li"><a class="nav-link">admin</a></router-link>
