22ebcb812c99c97306afb71f727a4ac68168c919
Danny Avery <danny.w.avery@gmail.com>
2018-06-09 09:14:23 -0600

    Add nested routes + router-links to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,15 +2,9 @@
 	<footer class="row">
 		<nav class="navbar navbar-toggleable-md navbar-light">
 			<ul class="nabar-nav">
-				<li class="nav-item">
-					<a href="" class="nav-link">home</a>
-				</li>
-				<li class="nav-item">
-					<a href="" class="nav-link">menu</a>
-				</li>
-				<li class="nav-item">
-					<a href="" class="nav-link">admin</a>
-				</li>
+				<router-link to="/" tag="li"><a class="nav-link">home</a></router-link>
+				<router-link to="/about" tag="li"><a class="nav-link">about</a></router-link>
+				<router-link to="/admin" tag="li"><a class="nav-link">admin</a></router-link>
 			</ul>
 		</nav>
 	</footer>
